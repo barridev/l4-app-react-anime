2f41fec43a3e6718142219abeff0ad1d56d7ab4f
Julien_dev <j_barri@etu-webschoolfactory.fr>
2022-06-16 14:37:00 +0200

    [EDIT] - ajout paragraphe index.js

--- .history/src/index_20220616142833.js
+++ .history/src/index_20220616142833.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <p>Demander nom de domaine</p>
+    <p></p>
     <App />
   </React.StrictMode>
 );
